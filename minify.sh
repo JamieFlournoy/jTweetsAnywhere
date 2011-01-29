be5fab62c894893914a198337c1e19cb805b6b64
JamieFlournoy <jamie@pervasivecode.com>
2011-01-28 18:32:37 -0800

    Move JS and CSS into lib/ subdir. Adjust minify.sh accordingly.

--- minify.sh
+++ minify.sh
@@ -5,6 +5,12 @@ if [ -z `ls tools/yuicompressor-*.jar` ]; then
     exit 1
 fi
 
+JS_LIB_NAME_PREFIX="jquery.jtweetsanywhere"
+JS_LIB_VERSION="1.2.1"
+SOURCE_JS=${JS_LIB_NAME_PREFIX}-${JS_LIB_VERSION}.js
+DEST_JS=${JS_LIB_NAME_PREFIX}-${JS_LIB_VERSION}.min.js
+
 YUI_COMPRESSOR_JAR=`ls tools/yuicompressor*.jar | head -1`
+
 echo "Minifying jTweetsAnywhere Using YUI Compressor at $YUI_COMPRESSOR_JAR"
-java -jar $YUI_COMPRESSOR_JAR jquery.jtweetsanywhere-1.2.1.js > jquery.jtweetsanywhere-1.2.1.min.js
+java -jar $YUI_COMPRESSOR_JAR lib/$SOURCE_Js > lib/$DEST_JS
