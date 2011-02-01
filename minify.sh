719fb0725b47c21d22d3678e1b3a001de59a1657
JamieFlournoy <jamie@pervasivecode.com>
2011-01-31 20:16:13 -0800

    Generalize minify to handle pluggable compressors.

--- minify.sh
+++ minify.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-if [ -z `ls tools/yuicompressor-*.jar` ]; then
-    echo "Download YUI Compressor from http://developer.yahoo.com/yui/compressor/ and put the JAR in the tools/ directory."
-    echo
-    exit 1
-fi
-
 JS_LIB_NAME_PREFIX="jquery.jtweetsanywhere"
 JS_LIB_VERSION="1.2.1"
 JS_LIB_DIR=lib
 SOURCE_JS=$JS_LIB_DIR/${JS_LIB_NAME_PREFIX}-${JS_LIB_VERSION}.js
 DEST_JS=$JS_LIB_DIR/${JS_LIB_NAME_PREFIX}-${JS_LIB_VERSION}.min.js
 
-YUI_COMPRESSOR_JAR=`ls tools/yuicompressor*.jar | head -1`
+COMPRESSOR_VARIANT="yui"
 
-echo "Minifying $SOURCE_JS using YUI Compressor at $YUI_COMPRESSOR_JAR"
-java -jar $YUI_COMPRESSOR_JAR -o $DEST_JS $SOURCE_JS
+if [[ "$COMPRESSOR_VARIANT" -eq "yui" ]]; then
+  COMPRESSOR_JAR=`ls tools/yuicompressor*.jar 2>/dev/null | head -1`
+  COMPRESSOR_ARGS="-o $DEST_JS $SOURCE_JS"
+  COMPRESSOR_NAME="YUI Compressor"
+  COMPRESSOR_URL="http://developer.yahoo.com/yui/compressor/"
+fi
+
+if [ -z $COMPRESSOR_JAR ]; then
+    echo "The $COMPRESSOR_NAME library is missing.\nDownload $COMPRESSOR_NAME from $COMPRESSOR_URL and put the JAR in the tools/ directory.\n\n"
+    exit 1
+fi
+
+echo "Minifying $SOURCE_JS using $COMPRESSOR_NAME at $YUI_COMPRESSOR_JAR"
+
+java -jar $COMPRESSOR_JAR $COMPRESSOR_ARGS
 
 ORIGINAL_BYTES=`du $SOURCE_JS | sed 's/lib.*//'`
 MINIFIED_BYTES=`du $DEST_JS | sed 's/lib.*//'`
